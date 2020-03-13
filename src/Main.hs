6cb14f0612069da893cca478871e702cfe5c0db8
napori0624 <naporin0624@gmail.com>
2020-03-13 18:23:55 +0000

    :+1: update editor

--- src/Main.hs
+++ src/Main.hs
@@ -3,7 +3,7 @@ main =
 
 quicksort :: (Ord a) => [a] -> [a]
 quicksort [] = []
-quicksort (x:xs) = 
+quicksort (x:xs) =
   let smallerOrEqual = [a | a <- xs, a > x]
       larger = [a | a<- xs, a <= x]
   in quicksort smallerOrEqual ++ [x] ++ quicksort larger
