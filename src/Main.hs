6e2f97c6a38f8dedc97e0d9c012f9cd68a9388ab
napori0624 <naporin0624@gmail.com>
2020-03-13 18:43:32 +0000

    :+1: maximum and replicate

--- src/Main.hs
+++ src/Main.hs
@@ -1,9 +1,25 @@
-main = 
-  print(quicksort [10, 1, 2, 3, 4, 5, 6, 7])
+main = do
+  print "HelloWorld"
+  let arr = [10, 2, 3, 1, 4, 5, 6, 7, -1]
+  print(quicksort arr)
+  print(maximum' arr)
+  print(replicate' 10 1)
+  print(replicate' 0 1)
+  
 
 quicksort :: (Ord a) => [a] -> [a]
 quicksort [] = []
 quicksort (x:xs) =
-  let smallerOrEqual = [a | a <- xs, a > x]
-      larger = [a | a<- xs, a <= x]
+  let smallerOrEqual = [a | a <- xs, a <= x]
+      larger = [a | a <- xs, a > x]
   in quicksort smallerOrEqual ++ [x] ++ quicksort larger
+
+maximum' :: (Ord a) => [a] -> a
+maximum' [] = error "maximum of empty list!"
+maximum' [x] = x
+maximum' (x:xs) = max x (maximum' xs)
+
+replicate' :: Int -> a -> [a]
+replicate' n x
+  | n <= 0  = []
+  | otherwise = x : replicate' (n - 1) x
